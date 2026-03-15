fd5a9fadb877e48de32c3dc150ef0d6ae5f023a3
mie-intel <paryapradhanika@gmail.com>
2026-03-15 21:14:03 +0700

    chores: tidy up the code

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import os
 from starlette.middleware.sessions import SessionMiddleware
 from sqlmodel import SQLModel
 
 from domain.users.routes import router as user_router
+from domain.groups.routes import router as group_router
 from database import engine
 
 
@@ -11,14 +13,17 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on startup
+    create_db_and_tables()
+    yield
+    # on shutdown
+
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware,
                    secret_key=os.getenv("SESSION_SECRET_KEY", "a_secret_key"))
-
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
 
 
 @app.get("/ping")
@@ -26,4 +31,5 @@ def ping():
     return {"message": "pong"}
 
 
-app.include_router(user_router)
+app.include_router(user_router, tags=["users"])
+app.include_router(group_router, prefix="/groups", tags=["groups"])
